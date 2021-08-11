c8565f5cb54a268a3670806483752f9ea0077fad
blaine-d-smith <blaine@thesmithstx.com>
2021-08-11 17:08:56 -0500

    Updated admin

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,1 +1,34 @@
-# from django.contrib import admin
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import User, UserManager
+from django.utils.translation import gettext as _
+
+
+class UserAdmin(BaseUserAdmin):
+    ordering = ['id']
+    list_display = ['email', 'name', 'last_login']
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (
+            _('Personal'),
+            {'fields': ('name',)}
+        ),
+        (
+            _('Permissions'),
+            {'fields': ('is_active', 'is_staff', 'is_superuser')}
+        ),
+        (
+            _('Dates'),
+            {'fields': ('last_login',)}
+        )
+    )
+    add_fieldsets = (
+        (None,
+         {'classes': ('wide',),
+          'fields': ('email', 'password1', 'password2',)}
+         ),
+    )
+
+
+# Register
+admin.site.register(User, UserAdmin)
