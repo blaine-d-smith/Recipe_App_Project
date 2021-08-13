24133720fbbbf803bd5f41a5b8f8c88346be44fb
blaine-d-smith <blaine@thesmithstx.com>
2021-08-13 15:28:25 -0500

    Updated Ingredients

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -5,9 +5,29 @@ from core.models import Tag, Ingredient
 from .serializers import TagSerializer, IngredientSerializer
 
 
-class TagViewSet(viewsets.GenericViewSet,
-                 mixins.ListModelMixin,
-                 mixins.CreateModelMixin):
+class BaseRecipeViewSet(viewsets.GenericViewSet,
+                        mixins.ListModelMixin,
+                        mixins.CreateModelMixin):
+    """
+    Viewset for recipe attributes.
+    """
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
+
+    def get_queryset(self):
+        """
+        Retrieves all objects associated with the authenticated user.
+        """
+        return self.queryset.filter(user=self.request.user).order_by('-name')
+
+    def perform_create(self, serializer):
+        """
+        Creates a new recipe.
+        """
+        serializer.save(user=self.request.user)
+
+
+class TagViewSet(BaseRecipeViewSet):
     """
     Viewset for displaying active tag data as JSON.
     """
@@ -17,22 +37,8 @@ class TagViewSet(viewsets.GenericViewSet,
     queryset = Tag.objects.all()
     serializer_class = TagSerializer
 
-    def get_queryset(self):
-        """
-        Retrieves all tag objects associated with the authenticated user.
-        """
-        return self.queryset.filter(user=self.request.user).order_by('-name')
 
-    def perform_create(self, serializer):
-        """
-        Creates a new tag.
-        """
-        serializer.save(user=self.request.user)
-
-
-class IngredientViewSet(viewsets.GenericViewSet,
-                        mixins.ListModelMixin,
-                        mixins.CreateModelMixin):
+class IngredientViewSet(BaseRecipeViewSet):
     """
     Viewset for displaying active ingredient data as JSON.
     """
@@ -41,16 +47,3 @@ class IngredientViewSet(viewsets.GenericViewSet,
     # Retrieves active ingredients.
     queryset = Ingredient.objects.all()
     serializer_class = IngredientSerializer
-
-    def get_queryset(self):
-        """
-        Retrieves all ingredient objects associated
-        with the authenticated user.
-        """
-        return self.queryset.filter(user=self.request.user).order_by('-name')
-
-    def perform_create(self, serializer):
-        """
-        Creates a new ingredient.
-        """
-        serializer.save(user=self.request.user)
